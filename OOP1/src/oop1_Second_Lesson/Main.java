ef8541eca53067049be28b3be28a6d7d1867b93e
M. Furkan Şahin <aliince.000000@gmail.com>
2022-10-15 15:24:12 +0300

    15.10.2022

---
+++ OOP1/src/oop1_Second_Lesson/Main.java
@@ -0,0 +1,10 @@
+package oop1_Second_Lesson;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("abc");
+	}
+
+}
---
+++ OOP1/src/oop1_Second_Lesson/Product.java
@@ -0,0 +1,11 @@
+package oop1_Second_Lesson;
+
+public class Product {
+	
+	String name;
+	double unitPrime; // Birim Fiyatı
+	double discount; // İndirim Oranı
+	String imageUrl;
+	int unitInStock;
+
+}
